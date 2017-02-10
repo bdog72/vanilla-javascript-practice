a5e96a74478e2a03d77b7a1217b832b40d2aa5cf
Brian Bycynski <high5bri@hotmail.com>
2017-02-10 07:16:31 -0500

    beginning practice

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  document.querySelector('h1').textContent += 'bdog'
 }
+
+const bdog2 = 'bdog2'
+
+console.log(bdog2)
 
 document.addEventListener('DOMContentLoaded', main)
 
