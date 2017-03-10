1545b09d2958201b1022d0822de8da8eb7781c30
Brian Bycynski <high5bri@hotmail.com>
2017-03-10 10:46:01 -0500

    finished 1st example

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
-const main = () => {
-  document.querySelector('h1').textContent += 'bdog'
-}
-
-function bdog1 () {
-  document.write('Bdoggy72')
-}
-bdog1()
-
-const timesFive = (num) => {
-  return num * 5
-}
-var answer = timesFive(5)
-document.write(answer)
-
 document.addEventListener('DOMContentLoaded', main)
 
 if (module.hot) {
